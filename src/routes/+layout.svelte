291ad65d2545ccfe7b88490ec5093174c27bd412
Kousher Alam <78843376+kousher-alam@users.noreply.github.com>
2022-12-21 17:17:40 +0600

    tailwind - svelte learning

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+      import "../app.scss";
+</script>
+<div>
+    <nav>
+        <ul>
+            <li><a href="/">Home</a></li>
+            <li><a href="/about">About</a></li>
+            <li><a href="/contact">Contact</a></li>
+        </ul>
+    </nav>
+</div>
+
+<div>
+    <h1 class="text-3xl font-bold underline text-red-400">
+        Hello world!
+      </h1>
+    <slot />
+</div>
---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+    const message = `About Page`;
+    let reactive = 0;
+    let userName = '';
+    const updateState = (event: PointerEvent) => {
+        reactive ++;
+        console.log(userName);
+    }
+</script>
+
+<style lang="scss">
+    h1 {
+        color: red;
+    }
+</style>
+
+<h1>{message}</h1>
+<input type="text" bind:value={userName}/>
+<p>{userName}</p>
+<button on:click={updateState}>{reactive}</button>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
